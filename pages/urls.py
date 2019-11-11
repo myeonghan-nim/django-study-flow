0121902cc33672c48e9eb91fa67a9edacd5cdc75
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-11-11 19:25:18 +0900

    Add: backend server

---
+++ pages/urls.py
@@ -0,0 +1,13 @@
+# process requests from master urls
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('ping/', views.ping),
+    path('pong/', views.pong),
+
+    path('post-ping/', views.post_ping),
+    path('post-pong/', views.post_pong),
+
+    path('static-example/', views.static_example),
+]
---
+++ posts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+
+def index(request):
+
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'index.html', context)
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    
+    post = Post()
+    post.title = title
+    post.content = content
+    post.save()
+
+    return render(request, 'create.html')
+    
