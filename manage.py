79f4174a941924355e452bf9bd98eebfc6419a9a
myeonghan-nim <ataraxia9408@gmail.com>
2021-12-18 22:38:09 +0900

    Change: project name

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advanced.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flow.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
